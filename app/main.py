ba1cba2a5dbbad7b68c9b8501d12aa6450936dad
Nick Clyde <nick@clyde.tech>
2024-06-11 09:15:21 -0700

    Add bundle endpoint for ValueSets (#12)

--- app/main.py
+++ app/main.py
@@ -11,11 +11,12 @@ from fastapi_cache.backends.redis import RedisBackend
 from fastapi_cache.decorator import cache
 
 from redis import asyncio as aioredis
+import requests
 
 
 @asynccontextmanager
 async def lifespan(_: FastAPI) -> AsyncIterator[None]:
-    redis = aioredis.from_url("redis://redis:6379")
+    redis = aioredis.from_url("redis://localhost:6379")
     FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
     yield
 
@@ -23,18 +24,41 @@ async def lifespan(_: FastAPI) -> AsyncIterator[None]:
 app = FastAPI(lifespan=lifespan)
 
 
-@cache()
-async def get_cache():
-    return 1
-
-
 @app.get("/")
 async def index():
     return dict(status="OK")
 
 
-@app.get("/blocking")
-@cache(namespace="test", expire=10)
-def blocking():
-    time.sleep(2)
-    return {"ret": 42}
+# Get https://phinvads.cdc.gov/baseStu3/ValueSet and return response
+@app.get("/phinvads/ValueSet")
+@cache(namespace="phinvads", expire=3600)
+def value_set(
+    name: str = None,
+    title: str = None,
+    identifier: str = None,
+    code: str = None,
+    version: str = None,
+    _getpages: str = None,
+):
+    url = "https://phinvads.cdc.gov/baseStu3/ValueSet"
+    params = {
+        "name": name,
+        "title": title,
+        "identifier": identifier,
+        "code": code,
+        "version": version,
+        "_getpages": _getpages,
+    }
+    return get(url, params)
+
+
+def get(url, params):
+    try:
+        response = requests.get(url, params=params)
+        res_type = response.headers.get("content-type")
+        if res_type == "application/json":
+            return response.json()
+        else:
+            return response.content
+    except Exception as e:
+        return {"error": str(e)}
