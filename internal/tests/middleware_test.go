377639ed2a34e275189cbdf164b2294e90bbb0cd
Amy Kintner <kintnerae@gmail.com>
2024-09-17 15:48:29 -0700

    add test utils

--- internal/tests/middleware_test.go
+++ internal/tests/middleware_test.go
@@ -25,33 +25,33 @@ func TestCommonHeaders(t *testing.T) {
 	// commonHeaders *returns* a http.Handler we can call its ServeHTTP()
 	// method, passing in the http.ResponseRecorder and dummy http.Request to
 	// execute it.
-	app.commonHeaders(next).ServeHTTP(rr, r)
+	commonHeaders(next).ServeHTTP(rr, r)
 	rs := rr.Result()
 	// Check that the middleware has correctly set the Content-Security-Policy header on the response.
 	expectedValue := "default-src 'self'; style-src 'self'"
-	assert.Equal(t, rs.Header.Get("Content-Security-Policy"), expectedValue)
+	Equal(t, rs.Header.Get("Content-Security-Policy"), expectedValue)
 	// Check that the middleware has correctly set the Referrer-Policy header on the response.
 	expectedValue = "origin-when-cross-origin"
-	assert.Equal(t, rs.Header.Get("Referrer-Policy"), expectedValue)
+	Equal(t, rs.Header.Get("Referrer-Policy"), expectedValue)
 	// Check that the middleware has correctly set the X-Content-Type-Options header on the response.
 	expectedValue = "nosniff"
-	assert.Equal(t, rs.Header.Get("X-Content-Type-Options"), expectedValue)
+	Equal(t, rs.Header.Get("X-Content-Type-Options"), expectedValue)
 	// Check that the middleware has correctly set the X-Frame-Options header on the response.
 	expectedValue = "deny"
-	assert.Equal(t, rs.Header.Get("X-Frame-Options"), expectedValue)
+	Equal(t, rs.Header.Get("X-Frame-Options"), expectedValue)
 	// Check that the middleware has correctly set the X-XSS-Protection header on the response
 	expectedValue = "0"
-	assert.Equal(t, rs.Header.Get("X-XSS-Protection"), expectedValue)
+	Equal(t, rs.Header.Get("X-XSS-Protection"), expectedValue)
 	// Check that the middleware has correctly set the Server header on the response.
 	expectedValue = "Go"
-	assert.Equal(t, rs.Header.Get("Server"), expectedValue)
+	Equal(t, rs.Header.Get("Server"), expectedValue)
 	// Check that the middleware has correctly called the next handler in line and the response status code and body are as expected.
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
+	Equal(t, rs.StatusCode, http.StatusOK)
 	defer rs.Body.Close()
 	body, err := io.ReadAll(rs.Body)
 	if err != nil {
 		t.Fatal(err)
 	}
 	body = bytes.TrimSpace(body)
-	assert.Equal(t, string(body), "OK")
+	Equal(t, string(body), "OK")
 }
---
+++ internal/tests/testutils_test.go
@@ -0,0 +1,56 @@
+package tests
+
+import (
+	"bytes"
+	"io"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/CDCgov/phinvads-fhir/internal/app"
+)
+
+// Create a newTestApplication helper which returns an instance of our
+// application struct containing mocked dependencies.
+func newTestApplication(t *testing.T) *Application {
+	return &Application{
+		logger: slog.New(slog.NewTextHandler(io.Discard, nil)),
+	}
+}
+
+// Define a custom testServer type which embeds a httptest.Server instance.
+type testServer struct {
+	*httptest.Server
+}
+
+// Create a newTestServer helper which initalizes and returns a new instance
+// of our custom testServer type.
+func newTestServer(t *testing.T, h http.Handler) *testServer {
+	ts := httptest.NewTLSServer(h)
+	return &testServer{ts}
+}
+
+// Implement a get() method on our custom testServer type. This makes a GET
+// request to a given url path using the test server client, and returns the
+// response status code, headers and body.
+func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, string) {
+	rs, err := ts.Client().Get(ts.URL + urlPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer rs.Body.Close()
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	body = bytes.TrimSpace(body)
+	return rs.StatusCode, rs.Header, string(body)
+}
+
+func Equal[T comparable](t *testing.T, actual, expected T) {
+	t.Helper()
+	if actual != expected {
+		t.Errorf("got: %v; want: %v", actual, expected)
+	}
+}
